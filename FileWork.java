e88954e49e3397175e8e6a80a9165380378f02d6
AndreyChernykov <61028728+AndreyChernykov@users.noreply.github.com>
2020-05-01 22:53:57 +0300

    Update FileWork.java

--- FileWork.java
+++ FileWork.java
@@ -1,22 +1,19 @@
+package pixelPaint;
 
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
 public class FileWork{
 	
-	static ArrayList<Pixel>pixelList = new ArrayList<Pixel>();//лист хранящий пиксели
-	
-	
 	public void fileWrite(String fileName) {//сохранение файла
 
 		int widthImg = 0;
 		int heightImg = 0;
-		for(Pixel p : pixelList) {//находим самые крайние пиксели по горизонтали и вертикали
+		for(Pixel p : PixelPanel.pixelList) {//находим самые крайние пиксели по горизонтали и вертикали
 			if(p.getX() + p.getSize() > widthImg) {
 				widthImg = p.getX() + p.getSize();
 			}
@@ -29,7 +26,7 @@ public class FileWork{
 			pict.createGraphics().drawImage(pict, 0, 0, null, null);
 						
 			Graphics2D graphics = (Graphics2D) pict.getGraphics();
-			for(Pixel p : pixelList) {//считываем из листа и записываем картинку в буффер
+			for(Pixel p : PixelPanel.pixelList) {//считываем из листа и записываем картинку в буффер
 				graphics.setColor(p.getColor());
 				graphics.fillRect(p.getX(), p.getY(), p.getSize(), p.getSize());
 			}
