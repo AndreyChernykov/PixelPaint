d72617211d04a58abf98c1ef087c78be3857ed7f
AndreyChernykov <61028728+AndreyChernykov@users.noreply.github.com>
2020-05-04 15:55:49 +0300

    Update ActLis.java

--- ActLis.java
+++ ActLis.java
@@ -11,31 +11,55 @@ import javax.swing.filechooser.FileSystemView;
 
 class ActLis implements ActionListener{//обработка Меню меню
 	
+	PixelPanel pixelPanel;
+	
+	ActLis(){
+		pixelPanel = new PixelPanel();
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e){
 		switch(e.getActionCommand()) {
 		case "Сохранить":
-			JFileChooser fch = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());//создаём окно выбора директории куда сохранять
-			fch.setFileFilter(new FileNameExtensionFilter("*.png", "*.*"));
-			int returnValue = fch.showSaveDialog(null);
-
-			if(returnValue == JFileChooser.APPROVE_OPTION) {
-				String fileName = fch.getSelectedFile().getName();
-				FileWork fileWork = new FileWork();
-				fileWork.fileWrite(fch.getCurrentDirectory() + "\\" + fileName);						
-			}
+			saveFile();
 			break;
 		case "Выход":
-			PixelPaint.frame.dispose();
+			exit();
 			break;
 		case "Очистить":
-			PixelPanel.pixelList.clear();
-			PixelPaint.frame.repaint();
+			clear();
 			break;
 		case "О программе":
-			JFrame frame = new JFrame();
-			JOptionPane.showMessageDialog(frame, "Пиксельная рисовалка v1.1 \n2020г.");
+			aboutApp();
 			break;
 		}			
+	}
+	
+	private void saveFile() {//сохранение файла
+		JFileChooser fch = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());//создаём окно выбора директории куда сохранять
+		fch.setFileFilter(new FileNameExtensionFilter("*.png", "*.*"));
+		int returnValue = fch.showSaveDialog(null);
+
+		if(returnValue == JFileChooser.APPROVE_OPTION) {
+			String fileName = fch.getSelectedFile().getName();
+			FileWork fileWork = new FileWork();
+			fileWork.fileWrite(fch.getCurrentDirectory() + "\\" + fileName);						
+		}
+	}
+	
+	private void exit() {//выход из программы
+		PixelPaint.frame.dispose();
+	}
+	
+	private void clear() {//очистка экрана
+		
+		pixelPanel.pixelList.clear();
+		PixelPaint.frame.repaint();
+	}
+	
+	private void aboutApp() {//о программе
+		String about = "Пиксельная рисовалка v1.1 \n2020г.";
+		JFrame frame = new JFrame();
+		JOptionPane.showMessageDialog(frame, about);
 	}
 }
