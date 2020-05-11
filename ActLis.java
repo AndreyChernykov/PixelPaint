79bddc741e52a8a2e89021fe31a8d4f3adf0a043
AndreyChernykov <61028728+AndreyChernykov@users.noreply.github.com>
2020-05-11 18:26:27 +0300

    Update ActLis.java

--- ActLis.java
+++ ActLis.java
@@ -2,6 +2,7 @@ package pixelPaint;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -10,18 +11,22 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.filechooser.FileSystemView;
 
 class ActLis implements ActionListener{//обработка Меню меню
-	
-	PixelPanel pixelPanel;
-	
+	FileWork fileWork;
 	ActLis(){
-		pixelPanel = new PixelPanel();
+		fileWork = new FileWork();
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e){
 		switch(e.getActionCommand()) {
 		case "Сохранить":
-			saveFile();
+			saveFile("*.png");
+			break;
+		case "Сохранить проект":
+			saveFile("*.pixArt");
+			break;
+		case "Загрузить проект":
+			loadFile();
 			break;
 		case "Выход":
 			exit();
@@ -35,30 +40,45 @@ class ActLis implements ActionListener{//обработка Меню меню
 		}			
 	}
 	
-	private void saveFile() {//сохранение файла
+	
+	private void saveFile(String txt) {//сохранение файла
 		JFileChooser fch = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());//создаём окно выбора директории куда сохранять
-		fch.setFileFilter(new FileNameExtensionFilter("*.png", "*.*"));
-		int returnValue = fch.showSaveDialog(null);
+		fch.setFileFilter(new FileNameExtensionFilter(txt, "*.*"));
+		int returnValue = fch.showDialog(null, "Сохранить файл");
 
 		if(returnValue == JFileChooser.APPROVE_OPTION) {
 			String fileName = fch.getSelectedFile().getName();
-			FileWork fileWork = new FileWork();
-			fileWork.fileWrite(fch.getCurrentDirectory() + "\\" + fileName);						
+			if(txt.equals("*.png")) {
+				fileWork.fileWrite(fch.getCurrentDirectory() + "\\" + fileName);// сохранение в png
+			}else {
+				fileWork.projectWrite(fch.getCurrentDirectory() + "\\" + fileName);	//сохранение проекта в pixArt
+			}									
 		}
 	}
 	
+	private void loadFile(){//загрузить проект
+		clear();
+		JFileChooser fileopen = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+		int ret = fileopen.showDialog(null, "Открыть файл");
+		if(ret == JFileChooser.APPROVE_OPTION){
+			File file = fileopen.getSelectedFile();
+			fileWork.projectRead(fileopen.getCurrentDirectory() + "\\" + file.getName());
+			
+		}
+	}
+		
 	private void exit() {//выход из программы
 		PixelPaint.frame.dispose();
 	}
 	
 	private void clear() {//очистка экрана
 		
-		pixelPanel.pixelList.clear();
+		PixelPanel.pixelList.clear();
 		PixelPaint.frame.repaint();
 	}
 	
 	private void aboutApp() {//о программе
-		String about = "Пиксельная рисовалка v1.1 \n2020г.";
+		String about = "Пиксельная рисовалка v1.2 \n2020г.";
 		JFrame frame = new JFrame();
 		JOptionPane.showMessageDialog(frame, about);
 	}
